72ecefb1eebd5864df47abba0ef017c6bfb0d452
abdullahmehboob20s <abdullahmehboob2002@gmail.com>
2024-09-17 20:08:12 +0500

    handlign error

--- app/routes/api.chat.ts
+++ app/routes/api.chat.ts
@@ -1,28 +1,32 @@
 import { openai } from "@ai-sdk/openai";
-import { ActionFunctionArgs } from "@remix-run/node";
+import { ActionFunctionArgs, json } from "@remix-run/node";
 import { streamText } from "ai";
 
 export const action = async ({ request }: ActionFunctionArgs) => {
   // Handle preflight OPTIONS request
-  const _a = (await request.json()).messages;
+  try {
+    const _a = (await request.json()).messages;
 
-  if (request.method === "OPTIONS") {
-    return new Response(null, {
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "POST, GET, OPTIONS",
-        "Access-Control-Allow-Headers": "Content-Type",
-      },
+    if (request.method === "OPTIONS") {
+      return new Response(null, {
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "POST, GET, OPTIONS",
+          "Access-Control-Allow-Headers": "Content-Type",
+        },
+      });
+    }
+
+    console.log("_a = ", _a);
+
+    const result = await streamText({
+      model: openai("gpt-4-turbo"),
+      system: `You are a helpful, respectful and honest assistant.`,
+      messages: _a,
     });
+
+    return result.toDataStreamResponse();
+  } catch (error) {
+    return json({ error });
   }
-
-  console.log("_a = ", _a);
-
-  const result = await streamText({
-    model: openai("gpt-4-turbo"),
-    system: `You are a helpful, respectful and honest assistant.`,
-    messages: _a,
-  });
-
-  return result.toDataStreamResponse();
 };
